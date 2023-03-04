8d2fddfd67cbd546fa58e16d0eca3b083d9c4e58
Teddy Porfiris <tporfiris@bayviewglen.ca>
2023-03-04 14:19:12 -0500

    vue router both APIs

---
+++ vue-class/vue3-intro/vue-4-router/vue-school-travel-app-comp/src/components/AppLink.vue
@@ -0,0 +1,22 @@
+<template>
+    <a 
+      v-if="isExternal" 
+      target="_blank"
+      rel="noopener"
+      class="external-link"
+      :href="to"><slot/></a>
+    <router-link v-else v-bind="$props" class="internal-link"><slot/></router-link>
+  </template>
+  <script setup>
+  import {RouterLink} from 'vue-router'
+  import { computed } from 'vue'
+
+  const props = defineProps({
+    ...RouterLink.props
+  })
+  
+
+  const isExternal = computed(()=>{
+    return typeof to === 'string' && to.startsWith('http')
+  })
+  </script>
---
+++ vue-class/vue3-intro/vue-4-router/vue-school-travel-app-comp/src/views/Protected.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>Greetings, {{username}}</h1>
+      <button @click="logout">Logout</button>
+      <router-link :to="{name: 'invoices'}">
+        <button>Invoices</button>
+      </router-link>
+    </div>
+  </template>
+  <script setup>
+  import { ref } from 'vue'
+
+  const username = ref(window.user)
+  const logout = ()=>{
+    window.user = null
+    this.$router.push({name: 'Home', query: {logout: null}})
+  }
+
+  </script>
